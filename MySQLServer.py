5c8fc30aeff27e202d2023d140ec12a1b682af3c
Karani <karanik509@gmail.com>
2025-08-17 09:50:31 +0300

    First_DB

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,7 +8,7 @@ try:
     mycursor = mydb.cursor()
     mycursor.Execute("""CREATE DATABASE IF NOT EXISTS alx_book_store""")
     print("Database 'alx_book_store' created successfully!")
-except:
+except mysql.connctor.Error:
     print("Could not connect to the database")
 mycursor.close()
 
