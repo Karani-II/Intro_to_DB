c62e4fc2239063b8ac6fb8332ef420ab62fe5e25
Karani <karanik509@gmail.com>
2025-08-17 09:48:27 +0300

    First_DB

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+import mysql.connector 
+mydb = mysql.connector.connect(
+    host = "local host",
+    user = "root",
+    password = "Karani8411@",
+)
+try:
+    mycursor = mydb.cursor()
+    mycursor.Execute("""CREATE DATABASE IF NOT EXISTS alx_book_store""")
+    print("Database 'alx_book_store' created successfully!")
+except:
+    print("Could not connect to the database")
+mycursor.close()
+
